b16d4377700c3282171de2c4714e9059035597fb
bobby.li <lihan0603@hupu.com>
2020-06-19 16:09:20 +0800

    🐸  [OMNI-DOOR/Tpl-Spa-React]: 0.3.22

--- packages/tpl-spa-react/src/templates/new/index.ts
+++ packages/tpl-spa-react/src/templates/new/index.ts
@@ -1,7 +1,7 @@
 import { tpl_engine_new } from '@omni-door/utils';
 
 const tpl = 
-`\`\${ts ? \`import * as \${componentName} from './\${componentName}'\` : '\`import \${componentName} from './\${componentName}'\`'};
+`\`\${ts ? import * as \${componentName} from './\${componentName}' : import \${componentName} from './\${componentName}'};
 
 export { \${componentName} } from './\${componentName}';
 export default \${componentName};
