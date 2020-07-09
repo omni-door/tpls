e1c8a7fea09fc765ad5c2dc595160df43c3ca56d
bobby.li <lihan0603@hupu.com>
2020-07-09 17:17:53 +0800

    🐸  [OMNI-DOOR/Tpl-Component-Library-React]: 0.4.16

--- packages/tpl-component-library-react/src/templates/storybook/webpack/index.ts
+++ packages/tpl-component-library-react/src/templates/storybook/webpack/index.ts
@@ -19,6 +19,18 @@ module.exports = {
         ],
         exclude: /node_modules/
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg|jpg|png|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'assets/[name].[ext]'
+            }
+          }
+        ]
+      },
       \${ts ? \`{
         test: /\.(ts|tsx)$/,
         use: [
