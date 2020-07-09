00d8f8f2ad8935924fe55c05e7e8431655b42629
bobby.li <lihan0603@hupu.com>
2020-07-09 15:46:00 +0800

    🐸  [OMNI-DOOR/Tpl-Spa-React]: 0.4.6

--- packages/tpl-spa-react/src/templates/source/html.ts
+++ packages/tpl-spa-react/src/templates/source/html.ts
@@ -5,7 +5,7 @@ const tpl =
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>\${project_name}</title>
   </head>
