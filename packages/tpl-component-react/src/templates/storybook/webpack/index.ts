6e1c24bad90e10292330aed914d7577a71df00aa
bobby.li <lihan0603@hupu.com>
2020-12-07 19:12:19 +0800

    🐸  [OMNI-DOOR/TPLS]: Upgrade templates: 1. publish tpl-component-react to 2.1.5;

--- packages/tpl-component-react/src/templates/storybook/webpack/index.ts
+++ packages/tpl-component-react/src/templates/storybook/webpack/index.ts
@@ -6,7 +6,6 @@ import rules_style_all from './rules_style_all';
 
 const tpl = 
 `\`\${use_strict}
-
 const path = require('path');
 
 module.exports = {
@@ -53,6 +52,9 @@ module.exports = {
   plugins: [],
   mode: 'development',
   resolve: {
+    alias: {
+      '@utils': path.resolve(__dirname, '../src/utils')
+    },
     extensions: [\${ts ? '".ts", ".tsx", ' : ''}".js", ".jsx", ".md", \${style ? (style === 'css' ? '".css"' : (style === 'less' ? '".less", ".css"' : '".scss", ".css", ".sass"')) : ''}]
   }
 };
