a14ad15673aecd39651660a7bc4329b4c081c78d
bobby.li <lihan0603@hupu.com>
2021-01-16 10:10:32 +0800

    🐸  [OMNI-DOOR/TPLS]: Upgrade templates: 1. publish tpl-spa-react to 2.2.5;

--- packages/tpl-spa-react/src/templates/webpack/dev/rules_style_all.ts
+++ packages/tpl-spa-react/src/templates/webpack/dev/rules_style_all.ts
@@ -8,11 +8,11 @@ const tpl =
             use: [
               'style-loader',
               { loader: 'css-loader', options: { modules: { localIdentName: '[local]___[hash:base64:6]' } } },
-              { loader: 'less-loader', options: { javascriptEnabled: true } }
+              { loader: 'less-loader', options: { lessOptions: { javascriptEnabled: true } } }
             ]
           },
           {
-            use: ['style-loader', 'css-loader', { loader: 'less-loader', options: { javascriptEnabled: true } }]
+            use: ['style-loader', 'css-loader', { loader: 'less-loader', options: { lessOptions: { javascriptEnabled: true } } }]
           }
         ]
       },
--- packages/tpl-spa-react/src/templates/webpack/prod/rules_style_all.ts
+++ packages/tpl-spa-react/src/templates/webpack/prod/rules_style_all.ts
@@ -8,11 +8,11 @@ const tpl =
             use: [
               MiniCssExtractPlugin.loader,
               { loader: 'css-loader', options: { modules: { localIdentName: '[local]___[hash:base64:6]' } } },
-              { loader: 'less-loader', options: { javascriptEnabled: true } }
+              { loader: 'less-loader', options: { lessOptions: { javascriptEnabled: true } } }
             ]
           },
           {
-            use: [MiniCssExtractPlugin.loader, 'css-loader', { loader: 'less-loader', options: { javascriptEnabled: true } }]
+            use: [MiniCssExtractPlugin.loader, 'css-loader', { loader: 'less-loader', options: { lessOptions: { javascriptEnabled: true } } }]
           }
         ]
       },
