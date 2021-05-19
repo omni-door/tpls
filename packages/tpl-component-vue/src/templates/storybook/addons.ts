3cf2e4cdda0ba39172418e476099671447857bb3
bobby.li <bobby.lhan@gmail.com>
2021-05-19 21:46:05 +0800

    🐸  [OMNI-DOOR/TPLS]: Upgrade templates: 1. publish tpl-component-vue to 2.4.15;

--- packages/tpl-component-vue/src/templates/storybook/addons.ts
+++ packages/tpl-component-vue/src/templates/storybook/addons.ts
@@ -7,6 +7,22 @@ import '@storybook/addon-notes/register';
 import 'storybook-readme/register';
 import '@storybook/addon-options/register';
 import '@storybook/addon-viewport/register';
+
+import { addons } from '@storybook/addons';
+
+addons.setConfig({
+  isFullscreen: false,
+  showNav: true,
+  showPanel: true,
+  panelPosition: 'right',
+  enableShortcuts: true,
+  isToolshown: true,
+  initialActive: 'sidebar',
+  sidebar: {
+    showRoots: false,
+    collapsedRoots: ['other']
+  }
+});
 \``;
 
 export const tpl_storybook_addons = {
