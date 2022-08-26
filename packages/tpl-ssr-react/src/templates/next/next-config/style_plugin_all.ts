5894d5248911a9b6980740860b08ba7f2e35cdfe
bobby.li <bobby.lhan@gmail.com>
2022-08-26 14:47:33 +0800

    🐸  [OMNI-DOOR/TPLS]: Upgrade templates: 1. publish tpl-ssr-react to 2.7.9;

--- packages/tpl-ssr-react/src/templates/next/next-config/style_plugin_all.ts
+++ packages/tpl-ssr-react/src/templates/next/next-config/style_plugin_all.ts
@@ -1,9 +1,9 @@
 const tpl = 
 `\`withLess({
-    cssModules: true,
-    cssLoaderOptions: {
-      importLoaders: 1,
-      localIdentName: '[local]___[hash:base64:5]'
+    lessLoaderOptions: {
+      lessOptions: {
+        javascriptEnabled: true
+      }
     }
   }),\``;
 
--- packages/tpl-ssr-react/src/templates/next/next-config/style_plugin_less.ts
+++ packages/tpl-ssr-react/src/templates/next/next-config/style_plugin_less.ts
@@ -1,12 +1,9 @@
 const tpl = 
 `\`withLess({
-    cssModules: true,
-    cssLoaderOptions: {
-      importLoaders: 1,
-      localIdentName: '[local]___[hash:base64:5]'
-    },
     lessLoaderOptions: {
-      javascriptEnabled: true
+      lessOptions: {
+        javascriptEnabled: true
+      }
     }
   }),\``;
 
