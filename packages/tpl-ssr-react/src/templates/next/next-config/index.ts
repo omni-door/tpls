cad54634290adf61928b30fc3e95f32efa256867
bobby.li <bobby.lhan@gmail.com>
2024-10-20 20:34:47 +0800

    🐸  [OMNI-DOOR/TPLS]: Upgrade templates: 1. publish tpl-ssr-react to 2.9.15;

--- packages/tpl-ssr-react/src/templates/next/next-config/index.ts
+++ packages/tpl-ssr-react/src/templates/next/next-config/index.ts
@@ -17,11 +17,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPlugin([
   withLess({
-    cssModules: true,
-    cssLoaderOptions: {
-      importLoaders: 1,
-      localIdentName: '[local]___[hash:base64:5]'
-    },
     lessLoaderOptions: {
       javascriptEnabled: true
     }
