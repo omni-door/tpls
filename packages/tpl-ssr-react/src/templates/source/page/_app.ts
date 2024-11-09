470539a4ddc74d68e90823f29e9c3ce1f22d581b
BobbyTalvin <bobby.talvin@gmail.com>
2024-11-09 17:24:34 +0800

    🐸  [OMNI-DOOR/TPLS]: Upgrade templates: 1. publish tpl-ssr-react to 2.9.23;

--- packages/tpl-ssr-react/src/templates/source/page/_app.ts
+++ packages/tpl-ssr-react/src/templates/source/page/_app.ts
@@ -3,8 +3,11 @@ import { tplEngineInit } from '@omni-door/utils';
 const tpl = 
 `\`import * as React from 'react';
 import '../src/styles/reset.\${style === 'all' ? 'scss' : style}';
+\${ts ? \`/* import types */
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }\${ts ? ': any' : ''}) {
+\` : ''}
+function MyApp({ Component, pageProps }\${ts ? ': AppProps' : ''}) {
   return <Component { ...pageProps } />;
 }
 
