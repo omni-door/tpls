f252ed317d224ad06cc34cdabc5a99449c352847
BobbyTalvin <bobby.talvin@gmail.com>
2025-02-13 13:24:21 +0800

    🐸  [OMNI-DOOR/TPLS]: Upgrade templates: 1. publish tpl-component-react to 3.0.3; 2. publish tpl-ssr-react to 3.0.6;

--- packages/tpl-component-react/src/templates/new/interface.ts
+++ packages/tpl-component-react/src/templates/new/interface.ts
@@ -1,11 +1,13 @@
 import { tplEngineNew } from '@omni-door/utils';
 
 const tpl = 
-`\`\${cc ? \`export interface \${componentName}States {}\n\` : ''}
+`\`\${ts ? \`import type { MouseEvent } from 'react';\` : ''}
+\${cc ? \`export interface \${componentName}States {}\n\` : ''}
+
 export interface \${componentName}Props {
   className?: string;
   prefixCls?: string;
-  onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => any;
+  onClick?: (\${ts ? \`e: MouseEvent<HTMLDivElement, MouseEvent>\` : 'e'}) => any;
 }
 \``;
 
