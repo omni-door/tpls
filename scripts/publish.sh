b786cdcaab6557c27d0abfc083952a534a2b2e5a
BobbyTalvin <bobby.talvin@gmail.com>
2026-01-19 23:04:57 +0800

    🐸  [OMNI-DOOR/TPLS]: Upgrade templates: 1. publish tpl-component-react to 3.2.1; 2. publish tpl-component-vue to 3.2.1; 3. publish tpl-spa-react to 3.2.1; 4. publish tpl-spa-react-pc to 3.2.1; 5. publish tpl-spa-vue to 3.2.1; 6. publish tpl-ssr-react to 3.2.1; 7. publish tpl-toolkit to 3.2.1;

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,7 +6,7 @@ if [ $? -eq 0 ]
 then
   pkjV=$(grep \"version\" package.json)
   version=$(echo ${pkjV} | tr -cd "[0-9].")
-  npm publish --access public --registry='https://registry.npmjs.org'
+  npm publish --access public
   echo -e "\033[32m \n${name}: The npm-package publish success - ${version}\n \033[0m"
 else
   echo -e "\033[31m \n${name}: The npm-package publish failed!\n \033[0m"
